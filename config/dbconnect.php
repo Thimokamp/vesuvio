3fd8ba11cfa5bd1bce2391100f0824cd110f915d
Thimo kamp <6036553@mborijnland.nl>
2025-12-11 10:51:02 +0100

    update reg systeem

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -28,6 +28,17 @@ class Database {
 $db = new Database();
 $conn = $db->connect();
 
+$sql = "SELECT id, naam, wachtwoord FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
 
 
 
